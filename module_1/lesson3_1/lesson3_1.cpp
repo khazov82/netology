3489a4ae23577b8f0797d99b8fb3943ca271747d
KhazovAV <khazov82@rambler.ru>
2026-03-25 01:00:43 +0600

    Добаил код задания 3_1

---
+++ module_1/lesson3_1/lesson3_1.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << boolalpha;
+	cout << "Оператор: ||" << endl;
+	cout << true  << '\t' << true  << '\t' << (true  || true)  << endl;
+	cout << false << '\t' << true  << '\t' << (false || true)  << endl;
+	cout << true  << '\t' << false << '\t' << (true  || false) << endl;
+	cout << false << '\t' << false << '\t' << (false || false) << endl;
+	cout << endl;
+
+	cout << "Оператор: &&" << endl;
+	cout << true  << '\t' << true  << '\t' << (true  && true)  << endl;
+	cout << false << '\t' << true  << '\t' << (false && true)  << endl;
+	cout << true  << '\t' << false << '\t' << (true  && false) << endl;
+	cout << false << '\t' << false << '\t' << (false && false) << endl;
+
+	return EXIT_SUCCESS;
+}
