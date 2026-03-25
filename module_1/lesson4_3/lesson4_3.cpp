39ab28a03e2644d58c1720a5a007603d50f1c7d1
KhazovAV <khazov82@rambler.ru>
2026-03-26 00:31:01 +0600

    Решение задачи 4_3. Таблица умножения для числа

---
+++ module_1/lesson4_3/lesson4_3.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int val = {};
+
+    cout << "Введите целое число:";
+    cin >> val;
+
+    cout << "Таблица умножения для этого числа:" << endl;
+
+    for (int mult = 1; mult <= 10; mult++)
+    {
+        printf("%d\tx\t%d\t=\t%d\n", val, mult, (val * mult));
+    }
+
+    return EXIT_SUCCESS;
+}
