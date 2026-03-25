2ce692accad5ad91380a92919114718033bdcf95
KhazovAV <khazov82@rambler.ru>
2026-03-25 23:45:20 +0600

    Добавил проект для задачи 4_1.

---
+++ module_1/lesson4_1/lesson4_1.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int sum = {};
+    int val = {};
+
+    do
+    {
+        cout << "Введите целое число или число '0', чтобы закончить:";
+        cin >> val;
+        sum += val;
+
+    } while (val);
+
+    cout << "Сумма:" << sum << endl;
+    
+}
