9a3cf4d9fe4239e8952d12e35a51d9a6fef873f0
KhazovAV <khazov82@rambler.ru>
2026-03-26 00:12:42 +0600

    Добавлен код для решения задания 4.2 - Сумма цифр числа

---
+++ module_1/lesson4_2/lesson4_2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int sum = {};
+    int val = {};
+
+    cout << "Введите целое число:";
+    cin  >> val;
+
+    while (val % 10)
+    {
+        sum += val % 10;
+        val /= 10;
+    }
+
+    cout << "Сумма:" << sum << endl;
+
+    return EXIT_SUCCESS;
+}
