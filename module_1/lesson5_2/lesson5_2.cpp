ac2c90f475e737d2fbaf9508d9f8dfd99f1cf187
KhazovAV <khazov82@rambler.ru>
2026-03-27 01:24:26 +0600

    Задача 5_2. Максимум и минимум

---
+++ module_1/lesson5_2/lesson5_2.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int ARR_SIZE = { 10 };
+    int array[ARR_SIZE];
+    int min, max;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    for (size_t i = 0; i < ARR_SIZE; i++)
+    {
+        array[i] = (i - 4) * 15;
+    }
+
+    cout << "Массив: ";
+    min = array[0];
+    max = array[0];
+
+    for (size_t i = 0; i < ARR_SIZE; i++)
+    {
+        cout << array[i] << " ";
+        min = (min > array[i]) ? array[i] : min;
+        max = (max < array[i]) ? array[i] : max;
+    }
+    cout << endl;
+
+    cout << "Минимальный элемент: "  << min << endl;
+    cout << "Максимальный элемент: " << max << endl;
+
+    return EXIT_SUCCESS;
+}
