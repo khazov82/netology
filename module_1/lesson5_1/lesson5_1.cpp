34977b3ca6cc47d3c55707660f0f9e4cc8c454c5
KhazovAV <khazov82@rambler.ru>
2026-03-27 01:01:20 +0600

    Решение задачи 5_1. Вывод массива

---
+++ module_1/lesson5_1/lesson5_1.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int ARR_SIZE = { 10 };
+    int array[ARR_SIZE];
+
+    // инициализация массива
+    for (size_t i = 0; i < ARR_SIZE; i++)
+    {
+        array[i] = (i + 1) * 10;
+    }
+
+    // вывод значений массива
+    for (size_t i = 0; i < ARR_SIZE; i++)
+    {
+        cout << array[i];
+        if ((i + 1) < ARR_SIZE)
+            cout << ", ";
+    }
+
+    return EXIT_SUCCESS;
+}
