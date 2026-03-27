cc717c09e0890ab051cb241503db511127d29c0d
KhazovAV <khazov82@rambler.ru>
2026-03-28 00:47:03 +0600

    Задача 5_3. Двумерный массив

---
+++ module_1/lesson5_3/lesson5_3.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <Windows.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int ROWS = { 3 };
+    const int COLS = { 6 };
+    int array[ROWS][COLS];
+    int min_i = 0;
+    int min_j = 0;
+    int max_i = 0;
+    int max_j = 0;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // инициализция массива случайными значениями
+    for (size_t i = 0; i < ROWS; i++)
+    {
+        for (size_t j = 0; j < COLS; j++)
+        {
+            array[i][j] = rand();
+        }
+    }
+
+    // поиск и вывод массива, также минимум и максимум
+    cout << "Массив: " << endl;
+
+    for (size_t i = 0; i < ROWS; i++)
+    {
+        for (size_t j = 0; j < COLS; j++)
+        {
+            if (array[min_i][min_j] > array[i][j])
+            {
+                min_i = i;
+                min_j = j;
+            }
+            if (array[max_i][max_j] < array[i][j])
+            {
+                max_i = i;
+                max_j = j;
+            }
+            cout << array[i][j] << '\t';
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout << "Индекс минимального элемента: "  << min_i  << " "  << min_j << endl;
+    cout << "Индекс максимального элемента: " << max_i  << " "  << max_j << endl;
+
+    return EXIT_SUCCESS;
+}
