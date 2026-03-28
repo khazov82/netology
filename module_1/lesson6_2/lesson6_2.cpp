3c172eada71d2348541eb1b8f6956fd59c0bcad4
KhazovAV <khazov82@rambler.ru>
2026-03-29 00:58:43 +0600

    Задача 6_2. Устранение дублирования

---
+++ module_1/lesson6_2/lesson6_2.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int pow(int val, int pow);
+
+int main(int argc, char** argv)
+{
+    int value = 5, power = 2;
+    int result = pow(value, power);
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << value << " в степени " << power << " = " << result << endl;
+
+    value = 3;
+    power = 3;
+    result = pow(value, power);
+
+    cout << value << " в степени " << power << " = " << result << endl;
+
+    value = 4;
+    power = 4;
+    result = pow(value, power);
+
+    cout << value << " в степени " << power << " = " << result << endl;
+
+    return EXIT_SUCCESS;
+}
+
+int pow(int value, int p)
+{
+    int result = value;
+
+    for (int i = 1; i < p; i++)
+    {
+        result *= value;
+    }
+
+    return result;
+}
