8ed2d4e4d92fee593d80d5463dda111b92f858c9
KhazovAV <khazov82@rambler.ru>
2026-03-29 23:34:29 +0600

    Задача 6_3. Числа Фибоначчи

---
+++ module_1/lesson6_3/lesson6_3.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+void fibo(int value);
+void fibo(int64_t a, int64_t b, int iter, int limit);
+
+int main(int argc, char** argv)
+{
+    int value;
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Введите число — номер элемента последовательности Фибоначчи: ";
+    cin >> value;
+    cout << endl << "Числа Фибоначчи: ";
+    fibo(value);
+
+    return EXIT_SUCCESS;
+}
+
+void fibo(int value)
+{
+    fibo(0, 1, 1, value);
+}
+
+void fibo(int64_t a, int64_t b, int iter, int limit)
+{
+    if (iter >= limit)
+        return;
+
+    if (iter == 1)
+        cout << "0 ";
+
+    cout << b << " ";
+
+    fibo(b, (a + b), ++iter, limit);
+}
