7ae3cbd5d4cf6119a437c07b94d428aacb53e28f
KhazovAV <khazov82@rambler.ru>
2026-03-30 19:57:32 +0600

    Задача 7_1. Адреса переменных

---
+++ module_1/lesson7_1/lesson7_1.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short Short;
+    int Int;
+    long Long;
+    long long LongLong;
+    float Float;
+    double Double;
+    bool Bool;
+
+    cout << "short: " << &Short << " " << sizeof(Short) << endl;
+    cout << "int: " << &Int   << " " << sizeof(Int) << endl;
+    cout << "long: " << &Long << " "  << sizeof(Long) << endl;
+    cout << "long long: " << &LongLong << " " << sizeof(LongLong) << endl;
+    cout << "float: " << &Float << " " << sizeof(Float) << endl;
+    cout << "double: " << &Double << " " << sizeof(Double) << endl;
+    cout << "bool: " << &Bool << " " << sizeof(Bool) << endl;
+
+    return EXIT_SUCCESS;
+}
