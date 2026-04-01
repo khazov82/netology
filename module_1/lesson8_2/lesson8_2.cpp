3ff0c7c3fc0e33c7b8beb5fafe1ddf7b30f378bf
KhazovAV <khazov82@rambler.ru>
2026-04-01 20:52:17 +0600

    Задача 2. Одинаковые, но разные функции

---
+++ module_1/lesson8_2/lesson8_2.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+namespace sum
+{
+    int calc(int x, int y)
+    {
+        return (x + y);
+    }
+}
+
+namespace diff
+{
+    int calc(int x, int y)
+    {
+        return (x - y);
+    }
+}
+
+namespace mult
+{
+    int calc(int x, int y)
+    {
+        return (x * y);
+    }
+}
+
+namespace division
+{
+    int calc(int x, int y)
+    {
+        return (x / y);
+    }
+}
+
+//------------------------------------------------------------------------------
+int main()
+{
+    int x{ 6 }, y{ 9 };
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Переменные: " << x << " и " << y << endl;
+
+    cout << "Сложение: " << sum::calc(x, y) << endl;
+    cout << "Вычитание: " << diff::calc(x, y) << endl;
+    cout << "Умножение: " << mult::calc(x, y) << endl;
+    cout << "Деление: " << division::calc(x, y) << endl;
+
+    return EXIT_SUCCESS;
+}
+//------------------------------------------------------------------------------
