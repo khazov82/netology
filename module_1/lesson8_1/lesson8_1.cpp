f61c7b93e91e6162c6c9c4dd4084e4e607fd5aef
KhazovAV <khazov82@rambler.ru>
2026-04-01 20:44:40 +0600

    Задача 1. Считающая функция

---
+++ module_1/lesson8_1/lesson8_1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+void counting_function();
+
+//------------------------------------------------------------------------------
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+
+    return EXIT_SUCCESS;
+}
+//------------------------------------------------------------------------------
+void counting_function()
+{
+    static int counter{ 1 };
+
+    cout << "Кол-во вызовов функции counting_function(): " << counter++ << endl;
+}
+//------------------------------------------------------------------------------
