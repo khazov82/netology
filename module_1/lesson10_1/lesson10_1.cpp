b64ced94e254128dfa1a4939f1b2418a353b736b
KhazovAV <khazov82@rambler.ru>
2026-04-12 00:05:55 +0600

    10_1. Ввод массива с консоли

---
+++ module_1/lesson10_1/lesson10_1.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int* create_array(int &size);
+void print_array(int *p_arr, int size);
+void delete_array(int *p_arr);
+
+//------------------------------------------------------------------------------
+int main()
+{
+	int* p_arr;
+	int  arr_size;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	p_arr = create_array(arr_size);
+	print_array(p_arr, arr_size);
+	delete_array(p_arr);
+}
+//------------------------------------------------------------------------------
+int* create_array(int &size)
+{
+	cout << "Введите размер массива:";
+	cin  >> size;
+
+	int* array { new int[size] };
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Введите " << (i + 1) << " элемент массива: " << endl;
+		cin >> array[i];
+	}
+
+	return array;
+}
+//------------------------------------------------------------------------------
+void print_array(int *p_arr, int size)
+{
+	cout << "Содержимое массива:";
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << p_arr[i] << " ";
+	}
+	cout << endl;
+}
+//------------------------------------------------------------------------------
+void delete_array(int *p_arr)
+{
+	delete[] p_arr;
+}
+//------------------------------------------------------------------------------
