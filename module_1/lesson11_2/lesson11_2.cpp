11844e84e4e3a8b06a3a40ebec3b6a25aa34f087
KhazovAV <khazov82@rambler.ru>
2026-04-13 21:30:00 +0600

    11_2. Угадайка

---
+++ module_1/lesson11_2/lesson11_2.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// программа сравнения строк разнами методами
+//------------------------------------------------------------------------------
+int main()
+{
+	string word_must = "малина";
+	string word_user = "";
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	do
+	{
+		cout << "Угадайте слово (ягода - м****а): ";
+		cin >> word_user;
+	} while (word_must != word_user);
+
+	cout << "Правильно! Вы угадали!" << endl;
+
+
+	word_must = "банан";
+
+	do
+	{
+		cout << "Угадайте слово (фрукт - б***н): ";
+		cin >> word_user;
+	} while (word_must.compare(word_user));
+
+	cout << "Правильно! Вы угадали!" << endl;
+
+}
+//------------------------------------------------------------------------------
