68f008fd6d4c6c48b5b306bd2fa16cbb7d86bb67
KhazovAV <khazov82@rambler.ru>
2026-04-16 21:16:09 +0600

    12_2. Чтение массива из файла

---
+++ module_1/lesson12_2/lesson12_2.cpp
@@ -0,0 +1,47 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int arrSize{ 0 };
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	ifstream file("in.txt");
+
+	if (!file)
+	{
+		cout << "Ошибка открытия файла!" << endl;
+		return EXIT_FAILURE;
+	}
+
+	file >> arrSize;
+
+	if (!arrSize)
+	{
+		cout << "Нулевой размер массива!" << endl;
+		return EXIT_FAILURE;
+	}
+
+	int* array = new int[arrSize];
+
+	for (int i = 0; i < arrSize; i++)
+	{
+		file >> array[i];
+	}
+
+	for (int i = arrSize - 1; i >= 0; i--)
+	{
+		cout << array[i] << " ";
+	}
+
+	file.close();
+	delete[] array;
+
+	return EXIT_SUCCESS;
+}
