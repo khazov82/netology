b1a0ee99de69b2d91ffa1e5ef65a3da7fb7d1cce
KhazovAV <khazov82@rambler.ru>
2026-04-16 20:57:40 +0600

    12_1. Вывод файла

---
+++ module_1/lesson12_1/lesson12_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string word = "";
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	ifstream file("in.txt");
+
+	if (!file)
+	{
+		cout << "Ошибка открытия файла!" << endl;
+		return EXIT_FAILURE;
+	}
+
+	while (file >> word)
+	{
+		cout << word << endl;
+	}
+
+	file.close();
+
+	return EXIT_SUCCESS;
+}
